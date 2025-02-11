697a98ca99d59059ea2d2feed3f2304315f89ddd
JavisOtieno <javisotieno@gmail.com>
2025-02-11 14:33:17 +0300

    trip fillable

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Trip extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'start_location',
+        'start_lat',
+        'start_long',
+        'end_location',
+        'end_lat',
+        'end_long',
+        'description',
+        'amount'
+    ];
 }
