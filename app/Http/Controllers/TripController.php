7aae1bf07b3e18e9fd50d3550d1fab4a9c3a8947
JavisOtieno <javisotieno@gmail.com>
2025-02-11 14:27:04 +0300

    trip table

---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trip;
+use Illuminate\Http\Request;
+
+class TripController extends Controller
+{
+    //
+    public function index(){
+
+        $trips = Trip::all();
+        return view('trip.index', ['trips'=>$trips]);
+    }
+}
---
+++ database/migrations/2025_02_11_141849_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('start_location');
+            $table->string('start_lat');
+            $table->string('start_long');
+            $table->string('end_location');
+            $table->string('end_lat');
+            $table->string('end_long');
+            $table->longText('description');
+            $table->unsignedBigInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
