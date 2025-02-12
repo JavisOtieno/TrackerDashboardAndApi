0b1e476587ed26194aa3ace3fd101a90fa5801e9
JavisOtieno <javisotieno@gmail.com>
2025-02-12 08:54:22 +0300

    testing

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\api\LocationController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-//test
+//testing
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
