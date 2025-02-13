51311ee69be5f80e50d95a4c4d91806139a38c33
JavisOtieno <javisotieno@gmail.com>
2025-02-13 09:45:29 +0300

    push 20 locations out of the total locations within the trip

--- resources/views/trip/index.blade.php
+++ resources/views/trip/index.blade.php
@@ -461,14 +461,16 @@ AIzaSyALLsNWwOC09xsRAqrK0S7dINi6BpNc7iw&callback=embedMap2"></script> --}}
 
         waypoints.push({ location: { lat: Number(trip.start_lat), lng: Number(
             trip.start_long) }, stopover: true });
-           var  count = 0;
-        trip.locations.forEach(location => {
-            console.log(location);
-            // Accessing outlet details
-            // console.log(tripitem.outlet);
-            count ++;
-            if(count<20){
-                waypoints.push({ location: { lat: Number(location.lat), lng: Number(location.long) }, stopover: true });
+        var  count = 0;
+        const totalLocations = trip.locations.length;
+        const step = Math.ceil(totalLocations / 20); // Calculate step size
+
+        trip.locations.forEach((location, index) => {
+            if (index % step === 0) { // Push every 'step'-th location
+                waypoints.push({
+                    location: { lat: Number(location.lat), lng: Number(location.long) },
+                    stopover: true
+                });
             }
         });
         waypoints.push({ location: { lat: Number(trip.end_lat), lng: Number(
