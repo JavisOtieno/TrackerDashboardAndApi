533454264b75374656aad2a5359462b7a335c093
JavisOtieno <javisotieno@gmail.com>
2025-02-13 15:34:52 +0300

    test modal title

--- resources/views/trip/index.blade.php
+++ resources/views/trip/index.blade.php
@@ -130,7 +130,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title">Map</h5>
-                            <div id="distance"></div>
+                            <div id="distance" class="modal-title"></div>
                             <button class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">×</span>
                                 </button>
@@ -539,7 +539,7 @@ AIzaSyALLsNWwOC09xsRAqrK0S7dINi6BpNc7iw&callback=embedMap2"></script> --}}
                     totalDistance += leg.distance.value; // distance.value is in meters
                 });
 
-                distanceDiv.innerText = "New Distance Value";
+                distanceDiv.innerText = "Distance :"+totalDistance;
 
                 
 
