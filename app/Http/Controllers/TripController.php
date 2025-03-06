ab4f90be8e651632f8c92e2b3140d53b31efa648
JavisOtieno <javisotieno@gmail.com>
2025-03-06 08:59:51 +0300

    trip selected distance removed

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -12,19 +12,19 @@ class TripController extends Controller
 
         // $trips = Trip::all();
         $trips = Trip::with('locations')->withSum('locations', 'distance')->orderBy('created_at', 'desc')->get();
-        $tripselected = Trip::with('locations')->find(17);
+        // $tripselected = Trip::with('locations')->find(17);
 
-        $totalDistance = 0;
+        // $totalDistance = 0;
 
-        $previousLocation = null;
-        foreach ($tripselected->locations as $location) {
-            if ($previousLocation) {
-                $totalDistance += abs($location->distance - $previousLocation->distance);
-            }
-            $previousLocation = $location;
-        }
+        // $previousLocation = null;
+        // foreach ($tripselected->locations as $location) {
+        //     if ($previousLocation) {
+        //         $totalDistance += abs($location->distance - $previousLocation->distance);
+        //     }
+        //     $previousLocation = $location;
+        // }
 
-        return $totalDistance;
+        // return $totalDistance;
         
         return view('trip.index', ['trips'=>$trips]);
     }
