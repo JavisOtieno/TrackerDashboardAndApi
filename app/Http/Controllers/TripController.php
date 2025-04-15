898342968b8e7ed23de49cb8223b40469cd54aee
Javis Otieno <javisotieno@gmail.com>
2025-04-15 12:40:47 +0300

    calculate distance on trip 19

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Trip;
+use App\Models\Location;
 use Illuminate\Http\Request;
 
 class TripController extends Controller
@@ -14,17 +15,55 @@ class TripController extends Controller
         $trips = Trip::with('locations')->withSum('locations', 'distance')->orderBy('created_at', 'desc')->get();
         $tripselected = Trip::with('locations')->find(19);
 
+        $locations = Location::where('trip_id','19')
+        ->orderBy('created_at', 'asc')
+        ->get();
+
         $totalDistance = 0;
 
-        $previousLocation = null;
-        foreach ($tripselected->locations as $location) {
-            if ($previousLocation) {
-                $totalDistance += abs($location->distance - $previousLocation->distance);
-            }
-            $previousLocation = $location;
+        function haversineDistance($lat1, $lon1, $lat2, $lon2, $unit = 'K')
+        {
+            $earthRadius = ($unit == 'K') ? 6371 : 3958.8; // Earth's radius in km or miles
+
+            $lat1 = deg2rad($lat1);
+            $lon1 = deg2rad($lon1);
+            $lat2 = deg2rad($lat2);
+            $lon2 = deg2rad($lon2);
+
+            $dLat = $lat2 - $lat1;
+            $dLon = $lon2 - $lon1;
+
+            $a = sin($dLat / 2) * sin($dLat / 2) +
+                cos($lat1) * cos($lat2) *
+                sin($dLon / 2) * sin($dLon / 2);
+
+            $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+            return $earthRadius * $c;
         }
 
-        return $totalDistance;
+        if ($locations->count() > 1) { // Ensure we have at least two locations
+            for ($i = 0; $i < count($locations) - 1; $i++) {
+                $totalDistance += haversineDistance(
+                    $locations[$i]->lat,
+                    $locations[$i]->long,
+                    $locations[$i + 1]->lat,
+                    $locations[$i + 1]->long
+                );
+            }
+        }
+
+        // $totalDistance = 0;
+
+        // $previousLocation = null;
+        // foreach ($tripselected->locations as $location) {
+        //     if ($previousLocation) {
+        //         $totalDistance += abs($location->distance - $previousLocation->distance);
+        //     }
+        //     $previousLocation = $location;
+        // }
+
+        // return $totalDistance;
         
         // return view('trip.index', ['trips'=>$trips]);
     }
