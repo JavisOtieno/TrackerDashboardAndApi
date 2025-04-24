ac363e290ec813578499859880418d38aebd7af3
javisotieno <javisotieno@gmail.com>
2025-04-24 15:44:24 +0300

    start movement stopover and end on one file

--- database/migrations/2025_04_16_120247_add_type_to_locations_table.php
+++ database/migrations/2025_04_16_120247_add_type_to_locations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('locations', function (Blueprint $table) {
-            $table->enum('type', ['movement', 'stop_over'])->default('movement')->after('distance');
+            $table->enum('type', ['start', 'movement', 'stopover', 'end'])->default('movement')->after('distance');
         });
     }
 
