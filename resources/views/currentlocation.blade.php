5675de8a48c3ec58570111c6f1cf83e3d661e97a
javisotieno <javisotieno@gmail.com>
2025-04-30 09:28:23 +0300

    test

--- resources/views/currentlocation.blade.php
+++ resources/views/currentlocation.blade.php
@@ -235,7 +235,58 @@
                 function processLocations(userswithcurrentlocations){
 
                     userswithcurrentlocations.forEach(user => {
-                        processLocation(user.latest_location)
+
+                    location = user.latest_location;
+                        
+                    const bounds = new google.maps.LatLngBounds();
+                    
+                    
+                    const lat1 = Number(location.lat);
+                    const long1 = Number(location.long);
+                    const position = { lat: lat1, lng: long1 };
+            
+                    // Create marker
+                    const marker = new google.maps.Marker({
+                        position: position,
+                        label: String(location.id),
+                        title: 'Location',
+                        zIndex: google.maps.Marker.MAX_ZINDEX + location.id,
+                        map: map
+                    });
+            
+                    // Create info window
+                    const date = new Date(location.created_at);
+                    // const username = location.user.name;
+                    const infowindow = new google.maps.InfoWindow({
+                        content: `
+                            <div>
+                                <h3>Date: ${date.toLocaleDateString('en-GB', {
+                                    day: 'numeric',
+                                    month: 'short',
+                                    year: 'numeric'
+                                })}</h3>
+                                <p>Time: ${date.toLocaleTimeString('en-GB', {
+                                    hour: '2-digit',
+                                    minute: '2-digit'
+                                })}</p>
+                                <p>Latitude: ${lat1}</p>
+                                <p>Longitude: ${long1}</p>
+                                
+                            </div>
+                        `
+                    });
+            
+                    // Add click listener
+                    marker.addListener('click', () => {
+                        infowindow.open(map, marker);
+                    });
+            
+                    markers.push(marker);
+                    bounds.extend(position);
+               
+            
+                    map.fitBounds(bounds);
+
                     });
                     
                 }
