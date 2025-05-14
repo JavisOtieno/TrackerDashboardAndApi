c30ddd01e80abe5a0e460fa4ab53c9147be1ee6a
javisotieno <javisotieno@gmail.com>
2025-05-14 14:37:36 +0300

    edit customer and driver

--- resources/views/driver/edit.blade.php
+++ resources/views/driver/edit.blade.php
@@ -29,7 +29,7 @@
             <!--<p>Use <code class="highlighter-rouge">.table-striped</code>to add zebra-striping to any table row within the <code class="highlighter-rouge">.tbody</code>.</p>-->
             
                 
-                    <form action="/saveeditdriver/{{$user['id']}}" method="POST" enctype="multipart/form-data">
+                    <form action="/saveeditdriver/{{$user['id']}}" method="POST"  enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <div class="">
@@ -51,7 +51,7 @@
                             @endif
                             <div class="form-group">
                                 <label for="name" class="form-label">Name</label>
-                                <input type="name" class="form-control" name="name" id="name" placeholder="Name" value="{{old('name',$user['name'])}}" autocomplete="name">
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="{{old('name',$user['name'])}}" autocomplete="name">
                             </div>
                             <div class="form-group">
                                 <label for="phone" class="form-label">Phone</label>
@@ -62,26 +62,22 @@
                                 <input type="text" class="form-control" name="email" id="email" placeholder="Email" value="{{old('email',$user['email'])}}" autocomplete="email">
                             </div>
                             {{-- <div class="form-group">
-                                <label for="admin" class="form-label">User Type</label>
+                                <label for="driver" class="form-label">User Type</label>
                                 <select name="usertype" class="form-control form-select select2" data-bs-placeholder="Select User Type">
-                                                    <option {{old('usertype',$user['usertype'])=="admin"? 'selected':''}} value="admin">Admin</option>
+                                                    <option {{old('usertype',$user['usertype'])=="driver"? 'selected':''}} value="driver">Driver</option>
                                                     <option {{old('usertype',$user['usertype'])=="sales"? 'selected':''}} value="sales">Sales</option>
                                 </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="target_group_id" class="form-label">Targets</label>
+                                <select name="target_group_id" class="form-control form-select select2" data-bs-placeholder="Select User Type">
+                                    <option {{old('target_group_id',$user['target_group_id'])=="0"? 'selected':''}} value="0">None</option>
+                                    @foreach($targetgroups as $targetgroup)
+                                        <option {{old('target_group_id',$user['target_group_id'])==$targetgroup['id']? 'selected':''}} value="{{$targetgroup['id']}}">{{$targetgroup['name']." - KSh ".number_format($targetgroup['amount'])}}</option>
+
+                                        @endforeach
+                </select>
                             </div> --}}
-                            <
-                           
-                            <div class="form-group">
-                                <label for="status" class="form-label">Status</label>
-                                <select name="status" class="form-control form-select select2" data-bs-placeholder="Select Status">
-                                    <option value="active" {{$user["status"]=='active'?'selected':'' }}>Active</option>
-                                    <option value="inactive" {{$user["status"]=='inactive'?'selected':'' }}>Inactive</option>
-                                </select>
-                            </div>
-                           
-                            <div class="form-group">
-                                <label for="dateTime" class="form-label">Date and Time Added</label>
-                                <input readonly type="text" class="form-control" name="dateTime" id="dateTime" placeholder="DateTime" value="{{date('d-M-Y H:i', strtotime($user['created_at']))}}" autocomplete="dateTime">
-                            </div>
                             <div class="form-group">
                                 <label for="image" class="form-label">Image (Optional)</label>
                                 {{-- {{url('storage/'.old('image',$user['image']))}} --}}
@@ -92,19 +88,17 @@
        
                                 </div>
                             </div>
-                            
+                            <div class="form-group">
+                                <label for="dateTime" class="form-label">Date and Time Added</label>
+                                <input readonly type="text" class="form-control" name="dateTime" id="dateTime" placeholder="DateTime" value="{{date('d-M-Y H:i', strtotime($user['created_at']))}}" autocomplete="dateTime">
+                            </div>
                             <div class="form-group">
                                 <label for="password" class="form-label">Password</label>
-                                <a href="/changepassword/driver/{{$user['id']}}" class="col-11 btn btn-info mb-0">Change Password</a>                            
-                            </div>
-                          
-
-                          
-
+                                <a href="/changepassword/driver/{{$user['id']}}" class="col-12 btn btn-info mb-0">Change Password</a>                            
                             </div>
                             
                         </div>
-                        <button class="btn btn-primary col-11 mt-4 mb-0">Save</button>
+                        <button class="btn btn-primary mt-4 mb-0">Save</button>
                     </form>
 
                     {{-- <div class="form-group" style="margin-top:20px;">
@@ -129,8 +123,8 @@
         <li style="margin-bottom: 10px;"><strong>Name</strong> : This field displays the name of the user and is editable.</li>
         <li style="margin-bottom: 10px;"><strong>Phone</strong> :This field displays the phone number of the user and is editable.</li>
         <li style="margin-bottom: 10px;"><strong>Email</strong> : This field displays the email address of the user and is editable.</li>
-        {{-- <li style="margin-bottom: 10px;"><strong>User Type</strong> : This field displays the type of user (Admin or Sales). It is a dropdown menu where you can select the user type.</li> --}}
-        <li style="margin-bottom: 10px;"><strong>Targets</strong> : This field displays the target group associated with the user. It is a dropdown menu where you can select the target group. </li>
+        {{-- <li style="margin-bottom: 10px;"><strong>User Type</strong> : This field displays the type of user (Driver or Sales). It is a dropdown menu where you can select the user type.</li>
+        <li style="margin-bottom: 10px;"><strong>Targets</strong> : This field displays the target group associated with the user. It is a dropdown menu where you can select the target group. </li> --}}
         <li style="margin-bottom: 10px;"><strong>Date and Time Added</strong> : This field displays the date and time when the user was added. It is disabled and cannot be modified.</li>
         <li style="margin-bottom: 10px;"><strong>Password</strong> : This field provides a button to change the user's password. Clicking on the button will redirect you to a page where you can change the password for the user.</li>
         </ol>
@@ -145,12 +139,3 @@
 </div>
 
 @endsection
-
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-
-    
-
-    
-    });
-    </script>
